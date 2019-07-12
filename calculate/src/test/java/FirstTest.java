825f975505cdffcb720987609c5f62939d9e754c
LexPalych <lexshcherbinin@gmail.com>
2019-07-12 15:28:11 +0300

    Initial commit

---
+++ calculate/src/test/java/FirstTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FirstTest {
+
+    @Test
+    void first() {
+        assertTrue(2 + 2 == 4);
+    }
+
+    @Test
+    void second() {
+        assertTrue(2 + 2 == 5);
+    }
+}
