80f60dedba8c3ad8ab716191c4624fe601096ac7
LexPalych <lexshcherbinin@gmail.com>
2019-10-14 09:51:53 +0300

    Удалил лишнее

--- calculate/src/main/java/CalculateString.java
+++ calculate/src/main/java/CalculateString.java
@@ -1,15 +1,11 @@
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-import static java.lang.Character.*;
+import static java.lang.Character.isDigit;
+import static java.lang.Character.isLetter;
 
 public class CalculateString {
     private static String expression;
-//    private static Map<Integer, Character> signList;
-//    private static Map<Integer, Double> numberList;
 
     public CalculateString(String expression) {
         this.expression = expression;
@@ -18,14 +14,6 @@ public class CalculateString {
     private static char getChar(int index) {
         return expression.charAt(index);
     }
-
-//    private static char getChar(int index, String expression) {
-//        return expression.charAt(index);
-//    }
-//
-//    private static Character getSign(int index) {
-//        return signList.get(index);
-//    }
 
     public double calculateString() {
         var check = new ExpressionValidation();
@@ -36,13 +24,6 @@ public class CalculateString {
         else
             throw new StringException("Какая-то неведомая ошибка");
     }
-
-//    static double calculate(final String expression) {
-//        numberList = createNumberList(expression);
-//        signList = createSignList(expression);
-//
-//        return getExpressionValue(1, signList.size());
-//    }
 
     static double calculate(final String expression) {
         List<Double> numberList = createNumberList(expression);
@@ -83,7 +64,7 @@ public class CalculateString {
         while (lastNumberIndex < expression.length() && getSymbolType(lastNumberIndex) == SymbolType.DIGIT) {
             lastNumberIndex++;
         }
-//        lastNumberIndex--;
+
         numberAsString = expression.substring(firstNumberIndex, lastNumberIndex);
         expressionElement.setNumber(Double.parseDouble(numberAsString));
         expressionElement.setLastSymbolIndex(lastNumberIndex-1);
@@ -138,7 +119,6 @@ public class CalculateString {
 
             if (symbolType == SymbolType.DIGIT) {
                 expressionElement = setNumberInExpressionElement(i);
-//                numberListSize++;
 
             } else if (symbolType == SymbolType.SIGN && i == 0) {
                 expressionElement.setNumber(0.0);
@@ -150,13 +130,11 @@ public class CalculateString {
 
             } else if (symbolType == SymbolType.LETTER) {
                 expressionElement = setFunctionInExpressionElement(i);
-//                numberListSize++;
 
             } else if (symbolType == SymbolType.BRACKET) {
                 closeBracketIndex = getClosingBracketIndex(i);
                 expressionElement.setNumber(calculate(subExpression.substring(i+1, closeBracketIndex-1)));
                 expressionElement.setLastSymbolIndex(closeBracketIndex);
-//                numberListSize++;
 
             } else if (symbol == '!') {
                 expressionElement.setNumber(factorial(numberList.get(numberList.size()-1)));
@@ -167,163 +145,6 @@ public class CalculateString {
         }
         return numberList;
     }
-
-//    private static double getExpressionValue(List<Double> numberList, List<Character> signList) {
-//        int i = 1;
-//        double value = numberList.get(0);
-//
-//        while (i < numberList.size()) {
-//            var number = numberList.get(i);
-//
-//            if (checkSignInList(i, '+', "+-")) {
-//                value += number;
-//
-//            } else if (checkSignInList(i, '-', "+-")) {
-//                value -= number;
-//
-//            } else if (checkSignInList(i, '*', "+-*/")) {
-//                value *= number;
-//
-//            } else if (checkSignInList(i, '/', "+-*/")) {
-//                if (number != 0)
-//                    value /= number;
-//                else
-//                    throw new ArithmeticException("Деление на ноль");
-//
-//            } else if (checkSignInList(i, '+', "*/^")) {
-//                value += getExpressionValue(++i, lastElement);
-//                break;
-//
-//            } else if (checkSignInList(i, '-', "*/^")) {
-//                var closeIndex = getLastIndex(i, lastElement);
-//
-//                value -= getExpressionValue(++i, closeIndex);
-//                i = closeIndex + 1;
-//                continue;
-//
-//            } else if (checkSignInList(i, '*', "^")) {
-//                value *= getExpressionValue(++i, i);
-//
-//            } else if (checkSignInList(i, '/', "^")) {
-//                var denominator = getExpressionValue(++i, i);
-//                if (denominator != 0)
-//                    value /= denominator;
-//                else
-//                    throw new ArithmeticException("Деление на ноль");
-//
-//            } else if (getSign(i) == '^') {
-//                value = Math.pow(value, numberList.get(i));
-//            }
-//            i++;
-//        }
-//
-//        return value;
-//    }
-
-//    /**
-//     * Расчитывает значение выражения в указанном интервале чисел и знаков
-//     * @param firstElement - индекс первых элементов из списков чисел и знаков
-//     * @param lastElement - индекс последних элементов из списков чисел и знаков
-//     * @return - возвращает значение выражения
-//     */
-//    private static double getExpressionValue(int firstElement, int lastElement) {
-//        var i = firstElement;
-//        var value = numberList.get(i-1);
-//
-//        while (i <= lastElement) {
-//            var number = numberList.get(i);
-//
-//            if (checkSignInList(i, '+', "+-")) {
-//                value += number;
-//
-//            } else if (checkSignInList(i, '-', "+-")) {
-//                value -= number;
-//
-//            } else if (checkSignInList(i, '*', "+-*/")) {
-//                value *= number;
-//
-//            } else if (checkSignInList(i, '/', "+-*/")) {
-//                if (number != 0)
-//                    value /= number;
-//                else
-//                    throw new ArithmeticException("Деление на ноль");
-//
-//            } else if (checkSignInList(i, '+', "*/^")) {
-//                value += getExpressionValue(++i, lastElement);
-//                break;
-//
-//            } else if (checkSignInList(i, '-', "*/^")) {
-//                var closeIndex = getLastIndex(i, lastElement);
-//
-//                value -= getExpressionValue(++i, closeIndex);
-//                i = closeIndex + 1;
-//                continue;
-//
-//            } else if (checkSignInList(i, '*', "^")) {
-//                value *= getExpressionValue(++i, i);
-//
-//            } else if (checkSignInList(i, '/', "^")) {
-//                var denominator = getExpressionValue(++i, i);
-//                if (denominator != 0)
-//                    value /= denominator;
-//                else
-//                    throw new ArithmeticException("Деление на ноль");
-//
-//            } else if (getSign(i) == '^') {
-//                value = Math.pow(value, numberList.get(i));
-//            }
-//            i++;
-//        }
-//
-//        return value;
-//    }
-
-//    /**
-//     * Находит индекс последнего знака, до которого нужно производить расчёт
-//     * @param firstIndex
-//     * @param lastIndex
-//     * @return
-//     */
-//    private static int getLastIndex(final int firstIndex, final int lastIndex) {
-//        var i = firstIndex;
-//
-//        while (true) {
-//            i++;
-//            var currentSign = getSign(i);
-//
-//            if (currentSign == '+' || currentSign == '-')
-//                return i-1;
-//
-//            else if (i == lastIndex)
-//                return i;
-//        }
-//
-//    }
-
-//    /**
-//     * Проверяет, что по определённому индексу находится определённый знак;
-//     * Проверяет, является ли этот знак последним;
-//     * Если нет, проверяет, содержится ли следующий за ним знак в определённом списке
-//     * @param index - индекс знака
-//     * @param currentSign - знак
-//     * @param signList - список знаков
-//     * @return - возвращает true, если по определённому индексу находитс определённый знак
-//     *          и следующий за ним знак находится в определённом списке знаков, либо если этот знак является последним
-//     */
-//    private static boolean checkSignInList(final int index, final char currentSign, final String signList) {
-//        var nextSign = getSign(index+1);
-//        if (nextSign != null) {
-//            var signInList =  signList
-//                    .chars()
-//                    .mapToObj(i -> (char) i)
-//                    .collect(Collectors.toList())
-//                    .contains(nextSign);
-//
-//            return (getSign(index) == currentSign) && signInList;
-//        }
-//
-//        return (getSign(index) == currentSign);
-//    }
 
     private static int getClosingBracketIndex(final int firstBracketIndex) {
         int bracketAmount = 0;
