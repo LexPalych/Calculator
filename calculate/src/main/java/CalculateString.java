2c4ab6a504b90890745a1f2252edea33326285b9
LexPalych <lexshcherbinin@gmail.com>
2019-10-19 22:13:02 +0300

    Вынес все четыре метода get...ExampleElement в ExampleElement

--- calculate/src/main/java/CalculateString.java
+++ calculate/src/main/java/CalculateString.java
@@ -4,9 +4,6 @@ import java.util.List;
 public class CalculateString {
     public static double calculateString(final String expression) {
         ExampleValidation check = new ExampleValidation();
-        ExampleElement exampleElement = new ExampleElement();
-
-        exampleElement.setExpression(expression);
 
         if (check.checkExpression(expression))
             return calculate(expression);
@@ -22,56 +19,56 @@ public class CalculateString {
         return CalculateExampleElement.calculateExpressionValue(numberList, signList);
     }
 
-    private static ExampleElement getNumberExampleElement(final String subExpression) {
-        int lastNumberIndex = 0;
-        String number;
-        ExampleElement exampleElement = new ExampleElement();
-
-        while (lastNumberIndex < subExpression.length() && SymbolValidation.getSymbolType(subExpression.charAt(lastNumberIndex)) == SymbolType.DIGIT) {
-            lastNumberIndex++;
-        }
-
-        number = subExpression.substring(0, lastNumberIndex);
-        exampleElement.setNumber(Double.parseDouble(number));
-        exampleElement.setLengthExample(number.length());
-
-        return exampleElement;
-    }
-
-    private static ExampleElement getFunctionExampleElement(final String subExpression) {
-        ExampleElement exampleElement = new ExampleElement();
-
-        int lastFunctionIndex = getClosingBracketIndex(subExpression);
-        String exampleFunction = subExpression.substring(0, lastFunctionIndex+1);
-        double functionValue = CalculateFunction.getFunctionValue(exampleFunction);
-
-        exampleElement.setNumber(functionValue);
-        exampleElement.setLengthExample(exampleFunction.length());
-
-        return exampleElement;
-    }
-
-    private static ExampleElement getSignExampleElement(final Character signChar) {
-        ExampleElement exampleElement = new ExampleElement();
-
-        exampleElement.setSign(signChar);
-        exampleElement.setLengthExample(1);
-
-        return exampleElement;
-    }
-
-    private static ExampleElement getBracketExampleElement(final String subExpression) {
-        ExampleElement exampleElement = new ExampleElement();
-
-        int lastFunctionIndex = getClosingBracketIndex(subExpression);
-        String exampleBracket = subExpression.substring(1, lastFunctionIndex);
-        double bracketValue = calculate(exampleBracket);
-
-        exampleElement.setNumber(bracketValue);
-        exampleElement.setLengthExample(exampleBracket.length() + 2);
-
-        return exampleElement;
-    }
+//    private static ExampleElement getNumberExampleElement(final String subExpression) {
+//        int lastNumberIndex = 0;
+//        String number;
+//        ExampleElement exampleElement = new ExampleElement();
+//
+//        while (lastNumberIndex < subExpression.length() && SymbolValidation.getSymbolType(subExpression.charAt(lastNumberIndex)) == SymbolType.DIGIT) {
+//            lastNumberIndex++;
+//        }
+//
+//        number = subExpression.substring(0, lastNumberIndex);
+//        exampleElement.setNumber(Double.parseDouble(number));
+//        exampleElement.setLength(number.length());
+//
+//        return exampleElement;
+//    }
+//
+//    private static ExampleElement getFunctionExampleElement(final String subExpression) {
+//        ExampleElement exampleElement = new ExampleElement();
+//
+//        int lastFunctionIndex = getClosingBracketIndex(subExpression);
+//        String exampleFunction = subExpression.substring(0, lastFunctionIndex+1);
+//        double functionValue = CalculateFunction.getFunctionValue(exampleFunction);
+//
+//        exampleElement.setNumber(functionValue);
+//        exampleElement.setLength(exampleFunction.length());
+//
+//        return exampleElement;
+//    }
+//
+//    private static ExampleElement getSignExampleElement(final Character signChar) {
+//        ExampleElement exampleElement = new ExampleElement();
+//
+//        exampleElement.setSign(signChar);
+//        exampleElement.setLength(1);
+//
+//        return exampleElement;
+//    }
+//
+//    private static ExampleElement getBracketExampleElement(final String subExpression) {
+//        ExampleElement exampleElement = new ExampleElement();
+//
+//        int lastFunctionIndex = getClosingBracketIndex(subExpression);
+//        String exampleBracket = subExpression.substring(1, lastFunctionIndex);
+//        double bracketValue = calculate(exampleBracket);
+//
+//        exampleElement.setNumber(bracketValue);
+//        exampleElement.setLength(exampleBracket.length() + 2);
+//
+//        return exampleElement;
+//    }
 
     private static List<Character> getSignList(final String subExpression) {
         List<Character> signList = new LinkedList<>();
@@ -106,13 +103,13 @@ public class CalculateString {
             SymbolType symbolType = SymbolValidation.getSymbolType(symbol);
 
             if (symbolType == SymbolType.DIGIT) {
-                exampleElement = getNumberExampleElement(subExpression.substring(i));
+                exampleElement = ExampleElement.getNumberExampleElement(subExpression.substring(i));
 
             } else if (symbolType == SymbolType.LETTER) {
-                exampleElement = getFunctionExampleElement(subExpression.substring(i));
+                exampleElement = ExampleElement.getFunctionExampleElement(subExpression.substring(i));
 
             } else if (symbolType == SymbolType.BRACKET) {
-                exampleElement = getBracketExampleElement(subExpression.substring(i));
+                exampleElement = ExampleElement.getBracketExampleElement(subExpression.substring(i));
 
             } else if (symbolType == SymbolType.SIGN) {
                 if (i == 0) {
@@ -123,12 +120,12 @@ public class CalculateString {
             }
 
             numberList.add(exampleElement.getNumber());
-            i += exampleElement.getLengthExample();
+            i += exampleElement.getLength();
         }
         return numberList;
     }
 
-    private static int getClosingBracketIndex(final String subExpression) {
+    static int getClosingBracketIndex(final String subExpression) {
         int bracketAmount = 0;
         int lastBracketIndex = 0;
         char currentChar;
--- calculate/src/main/java/ExampleElement.java
+++ calculate/src/main/java/ExampleElement.java
@@ -1,22 +1,14 @@
 public class ExampleElement {
-    private static String example;
-    private int lengthExample;
-
     private Character sign;
     private Double number;
-    private Double function;
-    private Double bracket;
+    private int length;
 
-    public void setExpression(String expression) {
-        this.example = expression;
+    public int getLength() {
+        return length;
     }
 
-    public int getLengthExample() {
-        return lengthExample;
-    }
-
-    public void setLengthExample(int lengthExample) {
-        this.lengthExample = lengthExample;
+    public void setLength(int length) {
+        this.length = length;
     }
 
     public Character getSign() {
@@ -35,19 +27,55 @@ public class ExampleElement {
         this.number = number;
     }
 
-    public Double getFunction() {
-        return function;
+    public static ExampleElement getNumberExampleElement(final String subExpression) {
+        int lastNumberIndex = 0;
+        String number;
+        ExampleElement exampleElement = new ExampleElement();
+
+        while (lastNumberIndex < subExpression.length() && SymbolValidation.getSymbolType(subExpression.charAt(lastNumberIndex)) == SymbolType.DIGIT) {
+            lastNumberIndex++;
+        }
+
+        number = subExpression.substring(0, lastNumberIndex);
+        exampleElement.setNumber(Double.parseDouble(number));
+        exampleElement.setLength(number.length());
+
+        return exampleElement;
     }
 
-    public void setFunction(Double function) {
-        this.function = function;
+    public static ExampleElement getFunctionExampleElement(final String subExpression) {
+        ExampleElement exampleElement = new ExampleElement();
+
+        int lastFunctionIndex = CalculateString.getClosingBracketIndex(subExpression);
+        String exampleFunction = subExpression.substring(0, lastFunctionIndex+1);
+        double functionValue = CalculateFunction.getFunctionValue(exampleFunction);
+
+        exampleElement.setNumber(functionValue);
+        exampleElement.setLength(exampleFunction.length());
+
+        return exampleElement;
     }
 
-    public Double getBracket() {
-        return bracket;
+    public static ExampleElement getSignExampleElement(final Character signChar) {
+        ExampleElement exampleElement = new ExampleElement();
+
+        exampleElement.setSign(signChar);
+        exampleElement.setLength(1);
+
+        return exampleElement;
     }
 
-    public void setBracket(Double bracket) {
-        this.bracket = bracket;
+    public static ExampleElement getBracketExampleElement(final String subExpression) {
+        ExampleElement exampleElement = new ExampleElement();
+
+        int lastFunctionIndex = CalculateString.getClosingBracketIndex(subExpression);
+        String exampleBracket = subExpression.substring(1, lastFunctionIndex);
+        double bracketValue = CalculateString.calculate(exampleBracket);
+
+        exampleElement.setNumber(bracketValue);
+        exampleElement.setLength(exampleBracket.length() + 2);
+
+        return exampleElement;
     }
+
 }
